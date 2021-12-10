6163a7dba6e018aeb8ebe675642ba1e581323595
Caleb <beerscb@gmail.com>
2021-12-10 15:52:39 -0500

    up to pg 91

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -3,6 +3,7 @@ defmodule RumblWeb.UserController do
 
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
+  plug :authenticate when action in [:index, :show]
 
   def new(conn, _params) do
     changeset = Accounts.change_registration(%User{}, %{})
@@ -30,4 +31,14 @@ defmodule RumblWeb.UserController do
     end
   end
 
+  defp authenticate(conn, opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to view this page. If you do not log in within 24 hours, a team of highly trained anthromorphic cacti will be dispatched to your location to deal with you personally. One of them is a twenty-foot tall saguaro named Lawrence. Lawrence is not terribly pleasant to deal with.")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
 end
